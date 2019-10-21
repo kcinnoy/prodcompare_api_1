c354a787aff8c56f31ec09cbb8b52cc0cb4346f5
kcinnoy <yonnick00@gmail.com>
2019-10-21 18:00:08 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             path: 'api/v1/users',
+             path_names: {
+                 sign_in: 'login',
+                 sign_out: 'logout',
+                 registration: 'signup'
+             },
+             controllers: {
+                 sessions: 'sessions',
+                 registrations: 'registrations'
+             },
+             defaults: { format: :json }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  namespace :api do
+    namespace :v1 do
+      resources :users, except: [:index] do
+        collection do
+          post 'login'
+        end
+      end
+
+      get '/products/', to: 'products#all'
+
+      get '/favorites/', to: 'favorites#index'
+      post 'favorites/', to: 'favorites#create'
+      delete '/favorites/:id', to: 'favorites#destroy'
+      # get '/favorites/:id', to: 'favorites#show'
+    end
+  end
 end
